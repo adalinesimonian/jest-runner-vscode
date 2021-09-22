1c60684daf59cdf66960db2553c6cfd74093575e
Adaline Valentina Simonian <adalinesimonian@gmail.com>
2021-09-22 14:51:23 -0700

    test: speed up command test, avoid openFolder

--- e2e/vscode-commands/__tests__/test.js
+++ e2e/vscode-commands/__tests__/test.js
@@ -1,16 +1,30 @@
 const path = require('path')
-const { commands, Uri, workspace, window } = require('vscode')
+const { commands, workspace, window } = require('vscode')
 
 const docPath = path.resolve(__dirname, '../test.json')
 
 describe('Describe', () => {
   it('should test', async () => {
-    await expect(
-      commands.executeCommand('vscode.openFolder', Uri.file(__dirname))
-    ).resolves.toBeUndefined()
-
     const doc = await workspace.openTextDocument(docPath)
     await window.showTextDocument(doc)
+
+    expect(doc.getText()).toMatchSnapshot()
+
+    await expect(
+      commands.executeCommand('editor.action.selectAll')
+    ).resolves.toBeUndefined()
+
+    await expect(
+      commands.executeCommand('editor.action.clipboardCopyAction')
+    ).resolves.toBeUndefined()
+
+    await expect(
+      commands.executeCommand('cursorRight')
+    ).resolves.toBeUndefined()
+
+    await expect(
+      commands.executeCommand('editor.action.clipboardPasteAction')
+    ).resolves.toBeUndefined()
 
     expect(doc.getText()).toMatchSnapshot()
   })
